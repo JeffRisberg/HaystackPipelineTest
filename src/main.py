9443c8e1ec001f29f4ff353cd5a7813660a761f1
JeffRisberg <jrisberg@aol.com>
2022-09-05 17:31:30 -0700

    set up test case

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ retriever_1 = FilterRetriever(document_store_1, scale_score = True)
 dicts_1 = [
   {
     'content': "Alpha",
-    'score': 0.5
+    'score': 0.55
   }
 ]
 document_store_1.write_documents(dicts_1)
@@ -19,7 +19,7 @@ retriever_2 = FilterRetriever(document_store_2, scale_score = True)
 dicts_2 = [
   {
     'content': "Beta",
-    'score': 0.5
+    'score': 0.54
   }
 ]
 document_store_2.write_documents(dicts_2)
@@ -31,6 +31,7 @@ retriever_3 = FilterRetriever(document_store_3, scale_score = True)
 dicts_3 = [
   {
     'content': "Gamma",
+    'score': 0.53
   }
 ]
 document_store_3.write_documents(dicts_3)
@@ -42,13 +43,22 @@ retriever_4 = FilterRetriever(document_store_4, scale_score = True)
 dicts_4 = [
   {
     'content': "Delta",
+    'score': 0.51
   }
 ]
 document_store_4.write_documents(dicts_4)
 
 
 documents = retriever_1.retrieve(query="Alpha Beta Gamma Delta")
-print(documents)
+for doc in documents:
+  print(doc)
+documents = retriever_2.retrieve(query="Alpha Beta Gamma Delta")
+for doc in documents:
+  print(doc)
+documents = retriever_3.retrieve(query="Alpha Beta Gamma Delta")
+for doc in documents:
+  print(doc)
+documents = retriever_4.retrieve(query="Alpha Beta Gamma Delta")
 for doc in documents:
   print(doc)
 
@@ -56,7 +66,11 @@ for doc in documents:
 pipeline = Pipeline()
 pipeline.add_node(component=retriever_1, name="Retriever1", inputs=["Query"])
 pipeline.add_node(component=retriever_2, name="Retriever2", inputs=["Query"])
-pipeline.add_node(component=JoinDocuments(join_mode="concatenate"), name="Join12", inputs=["Retriever1", "Retriever2"])
+pipeline.add_node(component=retriever_3, name="Retriever3", inputs=["Query"])
+pipeline.add_node(component=retriever_4, name="Retriever4", inputs=["Query"])
+pipeline.add_node(component=JoinDocuments(), name="Join12", inputs=["Retriever1", "Retriever2"])
+pipeline.add_node(component=JoinDocuments(), name="Join34", inputs=["Retriever3", "Retriever4"])
+pipeline.add_node(component=JoinDocuments(), name="JoinFinal", inputs=["Join12", "Join34"])
 
 
 res = pipeline.run(query="Alpha Beta Gamma Delta")
